a696a8895e34828ff26f72839b2829ba7f5eed3b
Alena Elegecheva <alena.elegecheva@gmail.com>
2023-04-30 17:16:51 +0700

    Исправить ошибки автотеста

--- routes/users.js
+++ routes/users.js
@@ -16,26 +16,20 @@ router.get('/me', getUser);
 
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
+    userId: Joi.string().hex().length(24).required(),
   }),
 }), getUserById); // возвращает пользователя по _id
 
 router.patch('/me', celebrate({
-  query: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24),
-  }),
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
 }), updateUser); // обновляет профиль
 
 router.patch('/me/avatar', celebrate({
-  query: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24),
-  }),
   body: Joi.object().keys({
-    avatar: Joi.string().regex(LINK),
+    avatar: Joi.string().required().regex(LINK),
   }),
 }), updateAvatar); //  обновляет аватар
 
